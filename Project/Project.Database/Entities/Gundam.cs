05972b8f1816a8e8a52de6e3e713cf944e147ed7
Offuru <andrei.rozmarin@gmail.com>
2024-04-18 11:10:57 +0300

    added entity classes

---
+++ Project/Project.Database/Entities/Gundam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Database.Entities
+{
+    public class Gundam : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Grade { get; set; }
+        public string Scale { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string Series {  get; set; }
+        public bool Available { get; set; }
+    }
+}
---
+++ Project/Project.Database/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Database.Entities
+{
+    public class User : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public decimal Balance { get; set; }
+        public List<Gundam> Gundams { get; set; }
+    }
+}
